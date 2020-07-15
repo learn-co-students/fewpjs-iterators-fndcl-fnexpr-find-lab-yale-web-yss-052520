7c5eb1d3d5af5a8f732ad24bb1ff673477b2806e
mlkelly <maurak87@gmail.com>
2020-07-15 17:02:51 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+// an array of objects
+function superbowlWin(array){
+  const winObj = array.find(object => object.result == "W")
+  return (winObj ? winObj.year : undefined )
+}
