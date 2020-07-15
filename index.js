c417b57984e18b685172c7d7b6905208f4acb16e
justin_james <jjames11799@gmail.com>
2020-07-15 16:45:08 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,18 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+// function superbowlWin(record) {
+    // return comboExistence()
+
+const superbowlWin = (record) => {
+  return comboExistence(record)
+}
+
+function comboExistence(record) {
+  if (record.find(combo => combo.result == 'W')) {
+    return record.find(combo => combo.result == 'W').year
+  } else {
+    return undefined
+  }
+}
