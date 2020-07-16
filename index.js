d0f11c87fc5cd83b8c5b2beaf06de1ad158c1014
dlee3427 <david.lee.sl2634@yale.edu>
2020-07-15 23:07:00 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(Array) {
+  let result = Array.find(record => record.result === "W")
+  if (result) {
+    return result.year
+  } else {
+    return undefined
+  }
+}
