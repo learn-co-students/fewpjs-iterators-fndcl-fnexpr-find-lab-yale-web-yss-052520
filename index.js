e30b5e5a43c2f6b5e19c47de8848d65a164b5395
ev275 <enrique.valencia@yale.edu>
2020-07-16 13:34:25 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  let win = array.find( object => object.result === "W")
+
+  if (win) {
+    return win.year
+  } else {
+    return undefined
+  }
+
+}
+
