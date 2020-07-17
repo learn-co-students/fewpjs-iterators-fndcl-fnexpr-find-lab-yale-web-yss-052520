ede140dded36472b81d2b1175970a612a0cf6a4f
alexisteh <wenhui.teh23@gmail.com>
2020-07-17 13:17:35 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(objects){
+  const game = objects.find(function(object){
+    if (object.result === "W"){
+      return true 
+    }
+  }) 
+  if (game == null){
+    return game 
+  } else {
+    return game.year 
+  }
+}
