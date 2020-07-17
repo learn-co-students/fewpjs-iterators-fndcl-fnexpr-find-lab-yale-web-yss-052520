db94d1f2a2a4dc56d3c34d4f7920b802098e5aa0
davidyu7 <david.yu@yale.edu>
2020-07-17 13:18:56 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+superbowlWin= (array) => {
+  let thing = array.find(game => {
+    return game.result === "W"
+  })
+  return thing ? thing.year : undefined
+}
+
+ssuperbowlWin = (record) => {
+  let result = record.find( record => record.result === "W" )
+  return !!result ? result.year : undefined
+}
