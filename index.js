58f503576f7db1fad1a3de53576d33c77f311ca2
CelestialEmperor <mapeiyao@gmail.com>
2020-07-18 21:41:06 +0100

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-const testVar = {}
+// const testVar = {}
+// const record = [
+//   { year: "2018", result: "L" },
+//   { year: "2017", result: "W" },
+//   { year: "2016", result: "N/A" }
+// ]
 
-function testFunc() {
-  return "hi"
+// function testFunc() {
+//   return "hi"
+// }
+
+function superbowlWin(record) {
+  let winningTeam = record.find( record => record.result === "W")
+  return winningTeam ? winningTeam.year : undefined
 }
