30816a8fd92634e5a1372982db9f42fceb65403f
KalebGz <kaleb.zeray@gmail.com>
2020-08-01 00:12:21 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arr){
+  const test = arr.find( function(ele)  {
+    return ele.result == 'W'
+  })
+  if(test){
+    return test.year
+  }else{
+    return test
+  }
+} 
