ea8949cae85febba1626688db02a8d586fa3f9bd
arthur318 <arthurhwang318@gmail.com>
2020-08-02 00:22:01 -0700

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-const testVar = {}
+// const testVar = {}
 
-function testFunc() {
-  return "hi"
+// function testFunc() {
+//   return "hi"
+// }
+
+function superbowlWin(array) {
+  let win = array.find( r => r.result === "W")
+  if (win == null) {
+    return undefined
+  }
+  return win.year
+
+  // can also do
+  // return !!result ? result.year : undefined
 }
+
+// const record = [
+//   {year: "2018", result: "L"},
+//   {year: "2017", result: "L"},
+//   {year: "2016", result: "N/A"}
+// ]
+
+// console.log(superbowlWin(record))
